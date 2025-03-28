0bb21b030d5851da9de5dada3dd4f5e92689e25d
gustavo domingues <deathdiec@gmail.com>
2025-03-27 23:47:46 -0300

    ADD start of the table for the DB

--- src/entity/editors/DBView.cpp
+++ src/entity/editors/DBView.cpp
@@ -1,13 +1,92 @@
 #include "DBView.hpp"
 #include "../../imgui/imgui_impl_opengl3.h"
+#include "../../tools/Logger.hpp"
 
 DBView::DBView(){
+  Table default_table = Table{
+    "default",
+    {
+      {"id", ColumnType::INT},
+      {"name", ColumnType::STRING},
+      {"position", ColumnType::VEC2},
+    },
+    {
+      {"1", "test", "id"},
+      {"2", "test2", "name"},
+      {"3", "test3", "position"},
+    }
+  };
+  Table test_table = Table{
+    "test",
+    {
+      {"id", ColumnType::INT},
+      {"name", ColumnType::STRING},
+      {"position", ColumnType::VEC2},
+    }
+  };
+  Table last_table = Table{
+    "last",
+    {
+    }
+  };
+
+  m_tables["default"] = default_table;
+  m_tables["test"] = test_table;
+  m_tables["last"] = last_table;
+}
+
+void DBView::TablesMenu(){
+  ImGui::Begin("Table View");
+  static char table_search[128] = "";
+  ImGui::InputText("##Search", table_search, IM_ARRAYSIZE(table_search));
+  for(auto& [name, table] : m_tables){
+    if(ImGui::CollapsingHeader(name.c_str())){
+      for(auto& column : table.columns){
+        ImGui::Text(column.name.c_str());
+      }
+      if(ImGui::Button(("access_" + name).c_str(), ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))){
+        table.is_open = true;
+      }
+    }
+    ImGui::Separator();
+  }
+  ImGui::Separator();
+  if (ImGui::Button("Create", ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))) {
+    ImGui::OpenPopup("Create Table");
+  }
+  ImGui::End();
+}
+
+void DBView::TablesWindow(){
+  for(auto& [name, table] : m_tables){
+    if(table.is_open){
+      ImGui::Begin((name + "_window").c_str(),  &table.is_open);
+      ImGui::BeginTable("table", table.columns.size());
+      for(auto& column : table.columns){
+        ImGui::TableSetupColumn(column.name.c_str());
+
+      }
+      ImGui::TableHeadersRow();
+      for(auto& column : table.columns){
+        for(auto& item : table.items){
+          if(column.name == item.column){
+            ImGui::TableNextColumn();
+            ImGui::Text(item.value.c_str());
+          }
+        }
+      }
+      ImGui::TableNextRow();
+      ImGui::EndTable();
+      ImGui::End();
+    }
+  }
 }
 
 void DBView::show(){
-  ImGui::Begin("Db View", nullptr, 0);
-  ImGui::Text("Hello World");
-  ImGui::End();
+  ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.05, 0.05, 0.05, 1.0));
+  TablesMenu();
+  TablesWindow();
+  ImGui::PopStyleColor();
 }
 
 void DBView::update(){
--- src/entity/editors/DBView.hpp
+++ src/entity/editors/DBView.hpp
@@ -2,6 +2,37 @@
 
 #include "IEditor.hpp"
 #include <string>
+#include <vector>
+#include <map>
+
+enum class ColumnType {
+  INT,
+  STRING,
+  FLOAT,
+  BOOL,
+  ARRAY,
+  VEC2,
+  VEC3
+};
+
+struct Item {
+  std::string name;
+  std::string value;
+  std::string column;
+};
+
+struct Column {
+  std::string name;
+  ColumnType type;
+};
+
+struct Table{
+  std::string name;
+  std::vector<Column> columns;
+  std::vector<Item> items;
+
+  bool is_open = false;
+};
 
 class DBView : public IEditor{
 public:
@@ -10,4 +41,10 @@ public:
   void update() override;
   void dispose() override;
   void draw() override;
+
+  void TablesMenu();
+  void TablesWindow();
+
+private:
+  std::map<std::string, Table> m_tables;
 };
