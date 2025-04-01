4735fea59f08215c11086006c1db574262d1b3da
gustavo domingues <deathdiec@gmail.com>
2025-03-31 23:05:46 -0300

    ADD started opups,  not wokring correctly yet

--- src/entity/editors/DBView.cpp
+++ src/entity/editors/DBView.cpp
@@ -35,14 +35,49 @@ DBView::DBView(){
   m_tables["last"] = last_table;
 }
 
+void DBView::collumn_popup(){
+  if(ImGui::BeginPopup("Create Table")){
+    static char column_name[128] = "";
+    static ColumnType column_type = ColumnType::INT;
+    ImGui::InputText("Column Name", column_name, IM_ARRAYSIZE(column_name));
+    ImGui::Combo("Column Type", (int*)&column_type, "INT\0STRING\0FLOAT\0BOOL\0ARRAY\0VEC2\0VEC3\0");
+    if(ImGui::Button("Create Column", ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))){
+      m_selected_table->columns.push_back({column_name, column_type});
+      ImGui::CloseCurrentPopup();
+    }
+    ImGui::EndPopup();
+  }
+}
+
+void DBView::item_popup(){
+  if(ImGui::BeginPopup("Create Item")){
+    static char item_name[128] = "";
+    static char item_value[128] = "";
+    ImGui::InputText("Item Name", item_name, IM_ARRAYSIZE(item_name));
+    ImGui::InputText("Item Value", item_value, IM_ARRAYSIZE(item_value));
+    if(ImGui::Button("Create Item", ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))){
+      auto item = Item{item_name, item_value, m_selected_column->name};
+      m_selected_table->items.push_back(item);
+      ImGui::CloseCurrentPopup();
+    }
+    ImGui::EndPopup();
+  }
+}
+
 void DBView::TablesMenu(){
   ImGui::Begin("Table View");
+  collumn_popup();
   static char table_search[128] = "";
   ImGui::InputText("##Search", table_search, IM_ARRAYSIZE(table_search));
   for(auto& [name, table] : m_tables){
     if(ImGui::CollapsingHeader(name.c_str())){
+
       for(auto& column : table.columns){
         ImGui::Text(column.name.c_str());
+      }
+      if(ImGui::Button("create new colunm", ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))){
+        m_selected_table = &table;
+        ImGui::OpenPopup("Create Table");
       }
       if(ImGui::Button(("access_" + name).c_str(), ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))){
         table.is_open = true;
@@ -62,20 +97,32 @@ void DBView::TablesWindow(){
     if(table.is_open){
       ImGui::Begin((name + "_window").c_str(),  &table.is_open);
       ImGui::BeginTable("table", table.columns.size());
+      item_popup();
       for(auto& column : table.columns){
         ImGui::TableSetupColumn(column.name.c_str());
-
       }
       ImGui::TableHeadersRow();
       for(auto& column : table.columns){
+        ImGui::TableNextColumn();
+        if(ImGui::Button(("create_" + column.name).c_str(), ImVec2(ImGui::GetContentRegionAvail().x - 10, 20))){
+          m_selected_column = &column;
+          m_selected_table = &table;
+          ImGui::OpenPopup("Create Item");
+        }
+        
+      }
+      for(int i = 0; i < table.columns.size(); i++){
+        ImGui::TableNextRow();
         for(auto& item : table.items){
-          if(column.name == item.column){
+          if(item.column == table.columns[i].name){
             ImGui::TableNextColumn();
-            ImGui::Text(item.value.c_str());
+            ImGui::Text(table.items[i].value.c_str());
           }
+
         }
       }
-      ImGui::TableNextRow();
+      //the same algiorithm but using the column in the for int 
+
       ImGui::EndTable();
       ImGui::End();
     }
@@ -86,6 +133,8 @@ void DBView::show(){
   ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.05, 0.05, 0.05, 1.0));
   TablesMenu();
   TablesWindow();
+
+
   ImGui::PopStyleColor();
 }
 
--- src/entity/editors/DBView.hpp
+++ src/entity/editors/DBView.hpp
@@ -42,9 +42,16 @@ public:
   void dispose() override;
   void draw() override;
 
+  void collumn_popup();
+  void item_popup();
+
   void TablesMenu();
   void TablesWindow();
 
 private:
   std::map<std::string, Table> m_tables;
+
+  Column* m_selected_column = nullptr;
+  Item* m_selected_item = nullptr;
+  Table* m_selected_table = nullptr;
 };
