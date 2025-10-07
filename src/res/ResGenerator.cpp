81f831dde4f3bfe5d01d123dfcf144f5bc29553c
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2025-10-07 10:00:57 -0300

    correctly auto generating resources

--- src/res/ResGenerator.cpp
+++ src/res/ResGenerator.cpp
@@ -1,5 +1,6 @@
 #include "ResGenerator.hpp"
 #include "../tools/Reader.hpp"
+#include "json.hpp"
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -26,6 +27,78 @@ std::vector<std::string> load_fonts(std::string path = "res/") {
   return fonts_names;
 }
 
+std::vector<std::string> load_sounds(std::string path = "res/") {
+  std::vector<std::string> sounds_names;
+  auto sounds = Reader::get_extension_files(path, ".wav");
+  for (auto sound : sounds) {
+    auto snd = sound.substr(sound.find_last_of("/\\") + 1);
+    snd = snd.substr(0, snd.find_last_of("."));
+    sounds_names.push_back(snd);
+  }
+  return sounds_names;
+}
+
+std::vector<std::string> load_data(std::string path) {
+  std::vector<std::string> data_names;
+  auto files = Reader::get_extension_files(path, ".json");
+
+  for (auto file : files) {
+    std::string path = file;
+    std::string file_name = path.substr(path.find_last_of("/\\") + 1);
+    file_name = file_name.substr(0, file_name.find_last_of("."));
+
+    std::string json = Reader::get_file_contents(file);
+    if (json.empty()) {
+      continue;
+    }
+
+    auto data = nlohmann::json::parse(json);
+
+    for (auto &value : data["data"]) {
+      try {
+        auto name = value["name"].get<std::string>();
+        auto folder = value["folder"].get<std::string>();
+
+        std::string data_name = folder + "/" + name;
+        data_names.push_back(data_name);
+
+      } catch (nlohmann::json::exception &e) {
+      }
+    }
+  }
+  return data_names;
+}
+
+std::vector<std::string> load_sprites(std::string path = "res/sprites") {
+  std::vector<std::string> sprite_names;
+  auto files = Reader::get_extension_files(path, ".json");
+
+  for (auto file : files) {
+    std::string path = file;
+    std::string file_name = path.substr(path.find_last_of("/\\") + 1);
+    file_name = file_name.substr(0, file_name.find_last_of("."));
+
+    std::string json = Reader::get_file_contents(file);
+    if (json.empty()) {
+      continue;
+    }
+
+    auto prefab = nlohmann::json::parse(json);
+
+    // loop through all the keys in the json array
+    // get all of the values from the json and try to create an Sprite from it
+    for (auto &[key, value] : prefab.items()) {
+      try {
+        auto name = value["name"].get<std::string>();
+        sprite_names.push_back(name);
+
+      } catch (nlohmann::json::exception &e) {
+      }
+    }
+  }
+  return sprite_names;
+}
+
 void res::generate_resources(std::string project_folder) {
   auto ase = load_aseprites(project_folder + "/res");
   std::string content;
@@ -39,12 +112,41 @@ void res::generate_resources(std::string project_folder) {
   }
   content += "}\n";
 
+  // start sprites
+  auto sprites = load_sprites(project_folder + "/res/sprites");
+  content += "namespace sprites {\n";
+  for (auto s : sprites) {
+    std::cout << "Sprite found: " << s << std::endl;
+    content += "inline constexpr const char* " + s + " = \"" + s + "\";\n";
+  }
+
   // start fonts
   auto fonts = load_fonts(project_folder + "/res/font");
   content += "namespace fonts {\n";
   for (auto f : fonts) {
     std::cout << "Font found: " << f << std::endl;
     content += "inline constexpr const char* " + f + " = \"" + f + "\";\n";
+  }
+  content += "}\n";
+
+  // start data
+  auto data = load_data(project_folder + "/res/data");
+  content += "namespace data {\n";
+  for (auto d : data) {
+    std::cout << "Data found: " << d << std::endl;
+    auto d_clean = d;
+    std::replace(d_clean.begin(), d_clean.end(), '/', '_');
+    content +=
+        "inline constexpr const char* " + d_clean + " = \"" + d + "\";\n";
+  }
+  content += "}\n";
+
+  // start sounds
+  auto sounds = load_sounds(project_folder + "/res/sounds");
+  content += "namespace sounds {\n";
+  for (auto s : sounds) {
+    std::cout << "Sound found: " << s << std::endl;
+    content += "inline constexpr const char* " + s + " = \"" + s + "\";\n";
   }
   content += "}\n";
 
